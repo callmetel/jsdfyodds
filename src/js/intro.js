fa5c61deb8abf086dc146bde5834a0ede2e71476
callmetel <info@callmetel.com>
2021-04-18 08:27:49 -0400

    Update Site BG & Design

--- src/js/intro.js
+++ src/js/intro.js
@@ -10,20 +10,20 @@ jQuery(document).ready(function($) {
 		intro = $('#background .mobile .jsgif:eq(0)');
 	};
 
-	introTL.set(shutoff,
-						{ alpha:1 }
-					)
-					.to(shutoff,
-						{ duration:.25, scale:.35, ease: "power4.inOut" }
-					)
-					.to(progress,
-						{ duration:.25, scale:0, height:0, ease: "power4.inOut" },
-						.5
-					)
-					.to(shutoff,
-						{ duration:.35, scale:0, ease: "power4.inOut", onComplete:startIntroVid },
-						.6
-					);
+	// introTL.set(shutoff,
+	// 					{ alpha:1 }
+	// 				)
+	// 				.to(shutoff,
+	// 					{ duration:.25, scale:.35, ease: "power4.inOut" }
+	// 				)
+	// 				.to(progress,
+	// 					{ duration:.25, scale:0, height:0, ease: "power4.inOut" },
+	// 					.5
+	// 				)
+	// 				.to(shutoff,
+	// 					{ duration:.35, scale:0, ease: "power4.inOut", onComplete:startIntroVid },
+	// 					.6
+	// 				);
 
 	function startIntroVid() {
 		// $('#intro-vid')[0].play();
